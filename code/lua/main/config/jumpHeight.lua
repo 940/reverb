a62bae1646c052c54d08ed7bb77264e53b2801bd
J <j_mahurin@yahoo.com>
2016-01-07 13:49:03 -0500

    Create jumpHeight.lua

---
+++ code/lua/main/config/jumpHeight.lua
@@ -0,0 +1,9 @@
+-- Basic parameters for the jpower function in the cmd, may be a bit buggy for now.
+while true do
+  game.Workspace.LocalPlayer.Humanoid - 
+    JumpHeight = %typeSet
+  when %typeSet = refer(reverb/code/cmd/main/config/jumpHeight) and find "typeSet"
+    loop()
+    end
+  end
+end
